0ca967d5cb27bfe34db6970720632f59d54a2aa5
default <yunus.emre.guzel@mths.ca>
2025-03-23 18:49:18 +0000

    changed the spelling

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
 // Created by: Emre Guzel
-// Created on: March 20  2025
+// Created on: March 23  2025
 // This file contains the JS functions for index.html
 
 "use strict"
